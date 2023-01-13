016102eb292abcf54e3599230f06706ad85b753d
snaco <jm.anderson.cs@gmail.com>
2023-01-12 20:06:03 -0500

    2.0.0 - add teleport protection, ability to disable welcome message globally

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "tech.snaco"
-version = "1.0-SNAPSHOT"
+version = "2.0.0"
 
 repositories {
     mavenCentral()
--- src/main/java/tech/snaco/SplitWorld/SplitWorld.java
+++ src/main/java/tech/snaco/SplitWorld/SplitWorld.java
@@ -2,7 +2,6 @@ package tech.snaco.SplitWorld;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.*;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -12,13 +11,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import tech.snaco.SplitWorld.utils.ItemStackArrayDataType;
 import tech.snaco.SplitWorld.utils.WorldConfig;
-import tech.snaco.SplitWorld.utils.mc;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -34,13 +32,22 @@ public class SplitWorld extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        default_game_mode = mc.gameModeFromString(config.getString("default_game_mode"));
+        default_game_mode = switch (config.getString("default_game_mode")) {
+            case "creative" -> GameMode.CREATIVE;
+            case "adventure" -> GameMode.ADVENTURE;
+            case "spectator" -> GameMode.SPECTATOR;
+            default -> GameMode.SURVIVAL;
+        };
         world_configs = config.getList("world_configs").stream().map(item -> new WorldConfig((Map<String, Object>) item)).collect(Collectors.toMap(WorldConfig::getWorldName, item -> item));
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        var welcome_message_disabled = config.getBoolean("disable_welcome_message");
+        if (welcome_message_disabled) {
+            return;
+        }
         var player = event.getPlayer();
         var player_pdc = player.getPersistentDataContainer();
         var world_name = player.getWorld().getName();
@@ -48,13 +55,31 @@ public class SplitWorld extends JavaPlugin implements Listener {
         if (!world_configs.containsKey(world_name) || !world_configs.get(world_name).enabled) {
             return;
         }
-        var world_config = getWorldConfigFromPlayer(player);
+        var world_config = getWorldConfig(player.getWorld());
         if (no_welcome == null) {
             player.sendMessage(Component.text("Hello " + player.getName() + "! "
                     + "This world is split! You can head over towards the " + world_config.creative_side
                     + " side of the border at " + world_config.border_axis + "=" + world_config.border_location
                     + " to enter the creative side of the world. Your inventory will automatically be saved"
                     + " and loaded whenever you cross the border. Have fun!"));
+        }
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent event) {
+        var destination = event.getTo();
+        var player = event.getPlayer();
+
+        if (locationInBufferZone(destination)) {
+            switchPlayerGameMode(player, GameMode.SPECTATOR);
+        } else if (locationOnCreativeSide(destination)) {
+            switchPlayerGameMode(player, GameMode.CREATIVE);
+        } else {
+            var needs_warp = player.getGameMode() != GameMode.SURVIVAL;
+            switchPlayerGameMode(player, GameMode.SURVIVAL);
+            if (needs_warp) {
+                warpPlayerToGround(player);
+            }
         }
     }
 
@@ -74,7 +99,7 @@ public class SplitWorld extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         var player = event.getPlayer();
-        managePlayerGameMode(player);
+        autoSetPlayerGameMode(player);
         convertBufferZoneBlocksAroundPlayer(player);
         if (playerInBufferZone(player)) {
             if (event.getTo().getBlock().getType() != Material.AIR) {
@@ -86,11 +111,11 @@ public class SplitWorld extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerWorldChange(PlayerChangedWorldEvent event) {
-        managePlayerGameMode(event.getPlayer());
+        autoSetPlayerGameMode(event.getPlayer());
     }
 
     public void convertBufferZoneBlocksAroundPlayer(Player player) {
-        var world_config = getWorldConfigFromPlayer(player);
+        var world_config = getWorldConfig(player.getWorld());
         var world = player.getWorld();
         var player_location = player.getLocation();
 
@@ -118,7 +143,7 @@ public class SplitWorld extends JavaPlugin implements Listener {
         }
     }
 
-    public void managePlayerGameMode(Player player) {
+    public void autoSetPlayerGameMode(Player player) {
         var player_world = player.getLocation().getWorld().getName();
         if (!world_configs.containsKey(player_world) || !world_configs.get(player_world).enabled) {
             switchPlayerGameMode(player, default_game_mode);
@@ -176,7 +201,7 @@ public class SplitWorld extends JavaPlugin implements Listener {
     }
 
     public void loadPlayerInventory(Player player, GameMode game_mode) {
-        var key = getKey(player, game_mode);
+        var key = getPlayerInventoryKey(player, game_mode);
         var player_pdc = player.getPersistentDataContainer();
         var new_inv = player_pdc.get(key, new ItemStackArrayDataType());
         if (new_inv != null) {
@@ -184,7 +209,7 @@ public class SplitWorld extends JavaPlugin implements Listener {
         }
     }
 
-    public NamespacedKey getKey(Player player, GameMode game_mode) {
+    public NamespacedKey getPlayerInventoryKey(Player player, GameMode game_mode) {
         return switch (game_mode) {
             case CREATIVE -> new NamespacedKey(this, player.getName() + "_creative_inv");
             case SURVIVAL -> new NamespacedKey(this, player.getName() + "_survival_inv");
@@ -194,59 +219,68 @@ public class SplitWorld extends JavaPlugin implements Listener {
     }
 
     public boolean playerOnCreativeSide(Player player) {
-        var world_config = getWorldConfigFromPlayer(player);
-        if (world_config.creative_side.equals("negative") && playerOnNegativeSideOfBufferZone(player)) {
+        return locationOnCreativeSide(player.getLocation());
+    }
+
+    public boolean locationOnCreativeSide(Location location) {
+        var world_config = getWorldConfig(location.getWorld());
+        if (world_config.creative_side.equals("negative") && locationOnNegativeSideOfBuffer(location)) {
             return true;
-        } else return world_config.creative_side.equals("positive") && playerOnPositiveSideOfBufferZone(player);
+        } else return world_config.creative_side.equals("positive") && locationOnPositiveSideOfBuffer(location);
     }
 
     public double getRelevantPlayerPos(Player player) {
-        var world_config = getWorldConfigFromPlayer(player);
-        return switch (world_config.border_axis) {
-            case "Y" -> player.getLocation().getY();
-            case "Z" -> player.getLocation().getZ();
-            default -> player.getLocation().getX();
-        };
+        return getRelevantPos(player.getLocation());
     }
 
     public boolean playerInBufferZone(Player player) {
-        var pos = getRelevantPlayerPos(player);
-        var world_config = getWorldConfigFromPlayer(player);
+        return locationInBufferZone(player.getLocation());
+    }
+
+    public boolean locationInBufferZone(Location location) {
+        var pos = getRelevantPos(location);
+        var world_config = getWorldConfig(location.getWorld());
         return pos > world_config.border_location - (world_config.border_width / 2.0)
             && pos < world_config.border_location + (world_config.border_width / 2.0);
     }
 
-    public boolean blockInBufferZone(Block block) {
-        var world_name = block.getWorld().getName();
-        if (!world_configs.containsKey(world_name) || !world_configs.get(world_name).enabled) {
-            return false;
-        }
-        var world_config = getWorldConfigFromChunk(block.getChunk());
-        var pos = switch (world_config.border_axis) {
-            case "Y" -> block.getY();
-            case "Z" -> block.getZ();
-            default -> block.getX();
-        };
-        return pos > world_config.border_location - (world_config.border_width / 2.0)
-            && pos < world_config.border_location + (world_config.border_width / 2.0);
-    }
+//    public boolean blockInBufferZone(Block block) {
+//        return locationInBufferZone(block.getLocation());
+//    }
+//
+//    public boolean playerOnNegativeSideOfBufferZone(Player player) {
+//        var world_config = getWorldConfig(player.getWorld());
+//        var pos = getRelevantPlayerPos(player);
+//        return pos < world_config.border_location - (world_config.border_width / 2.0);
+//    }
+//    public boolean playerOnPositiveSideOfBufferZone(Player player) {
+//        var pos = getRelevantPos(player.getLocation());
+//        var world_config = getWorldConfig(player.getWorld());
+//        return pos > world_config.border_location + (world_config.border_width / 2.0);
+//    }
 
-    public boolean playerOnNegativeSideOfBufferZone(Player player) {
-        var world_config = getWorldConfigFromPlayer(player);
-        var pos = getRelevantPlayerPos(player);
-        return pos < world_config.border_location - (world_config.border_width / 2.0);
-    }
-    public boolean playerOnPositiveSideOfBufferZone(Player player) {
-        var pos = getRelevantPlayerPos(player);
-        var world_config = getWorldConfigFromPlayer(player);
+    public boolean locationOnPositiveSideOfBuffer(Location location) {
+        var world_config = getWorldConfig(location.getWorld());
+        var pos = getRelevantPos(location);
         return pos > world_config.border_location + (world_config.border_width / 2.0);
     }
 
-    public WorldConfig getWorldConfigFromPlayer(Player player) {
-        return world_configs.get(player.getWorld().getName());
+    public boolean locationOnNegativeSideOfBuffer(Location location) {
+        var world_config = getWorldConfig(location.getWorld());
+        var pos = getRelevantPos(location);
+        return pos < world_config.border_location - (world_config.border_width / 2.0);
     }
 
-    public WorldConfig getWorldConfigFromChunk(Chunk chunk) {
-        return world_configs.get(chunk.getWorld().getName());
+    public double getRelevantPos(Location location) {
+        var world_config = getWorldConfig(location.getWorld());
+        return switch (world_config.border_axis) {
+            case "Y" -> location.getY();
+            case "Z" -> location.getZ();
+            default -> location.getX();
+        };
+    }
+
+    public WorldConfig getWorldConfig(World world) {
+        return world_configs.get(world.getName());
     }
 }
