a1c0eaa8a09e0c7d7997f078f25be6630f27a4d3
james <james@snaco.tech>
2025-09-06 21:02:53 -0400

    run server config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,44 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("java")
-    kotlin("jvm") version "1.8.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+  id("java")
+  kotlin("jvm") version "1.8.0"
+  id("com.github.johnrengelman.shadow") version "7.1.2"
+  id("xyz.jpenilla.run-paper") version "2.1.0"
 }
 
 group = "tech.snaco"
 version = "3.0.0"
 
 repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
+  mavenCentral()
+  maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    shadow(kotlin("stdlib-jdk8"))
+  implementation(kotlin("stdlib-jdk8"))
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+  compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+  shadow(kotlin("stdlib-jdk8"))
 }
 
 tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "17"
+  jvmTarget = "17"
 }
 
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "17"
+  jvmTarget = "17"
+}
+
+tasks {
+  runServer {
+    minecraftVersion("1.19.3")
+  }
 }
