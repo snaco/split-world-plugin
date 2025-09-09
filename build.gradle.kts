fc808c95f516b1298c36ec2c1767d79980b82c17
james <james@snaco.tech>
2025-09-08 20:15:45 -0400

    fix missing shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "tech.snaco"
-version = "3.1.0"
+version = "3.1.1+1.19"
 
 repositories {
   mavenCentral()
@@ -20,7 +20,6 @@ dependencies {
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
   compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-  shadow(kotlin("stdlib-jdk8"))
 }
 
 tasks.getByName<Test>("test") {
@@ -41,4 +40,12 @@ tasks {
   runServer {
     minecraftVersion("1.19.3")
   }
+}
+
+tasks {
+  shadowJar {
+    archiveClassifier.set("")
+    exclude("META-INF/*.kotlin_module")
+  }
+  build { dependsOn(shadowJar) }
 }
